9f79d0bff831926c3616e2fb060edb79417e1968
Ankit-Flutch <ankitmishra2108@gmail.com>
2023-10-15 21:56:36 +0530

    API for product Creation.

--- producer/controllers/controller.go
+++ producer/controllers/controller.go
@@ -1,9 +1,57 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"net/http"
 
-func CreateProduct (c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "product Created",
+	"github.com/ankitdmon/producer/initializers"
+	"github.com/ankitdmon/producer/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateProduct(c *gin.Context) {
+	var product models.Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Convert to JSON Array
+	productImagesJSON, err := json.Marshal(product.ProductImages)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to process product images",
+		})
+		return
+	}
+
+	// Convert to JSON array
+	compressedImagesJSON, err := json.Marshal(product.CompressedImages)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to process compressed product images",
+		})
+		return
+	}
+
+	product.ProductImages = []string{string(productImagesJSON)}
+	product.CompressedImages = []string{string(compressedImagesJSON)}
+
+	db := initializers.DB
+
+	result := db.Create(&product)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to create product",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Product Created",
+		"product": product,
 	})
 }
--- producer/models/product.go
+++ producer/models/product.go
@@ -4,11 +4,12 @@ import "time"
 
 type Product struct {
 	ProductID          int       `json:"product_id" gorm:"primaryKey"`
+	UserID             int       `json:"user_id" `
 	ProductName        string    `json:"product_name"`
 	ProductDescription string    `json:"product_description"`
-	ProductImages      []string  `json:"product_images" gorm:"type:LONGTEXT[]"`
+	ProductImages      []string  `json:"product_images" gorm:"type:text[]"`
 	ProductPrice       float64   `json:"product_price"`
-	CompressedImages   []string  `json:"compressed_product_images" gorm:"type:LONGTEXT[]"`
+	CompressedImages   []string  `json:"compressed_product_images" gorm:"type:text[]"`
 	CreatedAt          time.Time `json:"created_at"`
 	UpdatedAt          time.Time `json:"updated_at"`
 }
