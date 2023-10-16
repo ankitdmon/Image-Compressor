0d38b35ce28780256bcc19ff6cd8461d0641322f
Ankit-Flutch <ankitmishra2108@gmail.com>
2023-10-16 10:34:05 +0530

    user test file.

---
+++ producer/test/user_test.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/ankitdmon/producer/models"
+)
+
+func TestUserModelValidation(t *testing.T) {
+	user := models.User{
+		Name:      "John Doe",
+		Mobile:    "123-456-7890",
+		Latitude:  37.7749,
+		Longitude: 122.4194,
+	}
+
+	if user.ID != 0 {
+		t.Errorf("Expected ID to be 0, but got %d", user.ID)
+	}
+
+	if user.Name != "John Doe" {
+		t.Errorf("Expected Name to be 'John Doe', but got '%s'", user.Name)
+	}
+
+	if user.Mobile != "123-456-7890" {
+		t.Errorf("Expected Mobile to be '123-456-7890', but got '%s'", user.Mobile)
+	}
+
+	if user.Latitude != 37.7749 {
+		t.Errorf("Expected Latitude to be 37.7749, but got %f", user.Latitude)
+	}
+
+	if user.Longitude != 122.4194 {
+		t.Errorf("Expected Longitude to be 122.4194, but got %f", user.Longitude)
+	}
+
+	if !user.CreatedAt.IsZero() {
+		t.Errorf("Expected CreatedAt to be zero, but got a non-zero timestamp")
+	}
+
+	if !user.UpdatedAt.IsZero() {
+		t.Errorf("Expected UpdatedAt to be zero, but got a non-zero timestamp")
+	}
+}
