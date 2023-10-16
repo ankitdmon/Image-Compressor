80ceeb36ab7433f20de186e16c4a411a23c9a593
Ankit-Flutch <ankitmishra2108@gmail.com>
2023-10-17 01:10:09 +0530

    ResizeAndCompressImage func.

--- consumer/utils/imageUtils.go
+++ consumer/utils/imageUtils.go
@@ -2,8 +2,12 @@ package utils
 
 import (
 	"fmt"
-
+	"image"
+	"image/jpeg"
 	"net/http"
+
+	"strings"
+
 	"github.com/nfnt/resize"
 	"github.com/sirupsen/logrus"
 )
@@ -28,3 +32,16 @@ func DownloadImage(imgURL string) (image.Image, error) {
 	logrus.Info("Image downloaded successfully from URL")
 	return img, nil
 }
+
+func ResizeAndCompressImage(img image.Image, quality int) ([]byte, error) {
+	imgResized := resize.Resize(1024, 0, img, resize.Lanczos3)
+
+	buffer := new(strings.Builder)
+	err := jpeg.Encode(buffer, imgResized, &jpeg.Options{Quality: quality})
+	if err != nil {
+		return nil, fmt.Errorf("failed to resize and compress image: %v", err)
+	}
+	logrus.Info("Image resized and compressed successfully")
+	return []byte(buffer.String()), nil
+}
+
